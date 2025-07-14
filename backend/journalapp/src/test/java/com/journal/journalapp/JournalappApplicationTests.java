33895fd4dca929b1627de094b6d3e987ac6dd485
Ken <kmparks60@gmail.com>
2025-07-14 13:29:25 -0400

    Spring.io project initialized and successfully run.

---
+++ backend/journalapp/src/test/java/com/journal/journalapp/JournalappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.journal.journalapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
